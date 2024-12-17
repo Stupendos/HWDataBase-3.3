f1e04068ceb516b32cb4afe8908f1603d8934a6c
Stupendos <topakovdenis671@gmail.com>
2024-12-17 19:55:42 +0700

    Start

---
+++ src/main/java/org/example/databasehw/DataBaseHwApplication.java
@@ -0,0 +1,13 @@
+package org.example.databasehw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataBaseHwApplication.class, args);
+    }
+
+}
