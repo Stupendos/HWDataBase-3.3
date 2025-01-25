441bf0c0c4b79c8ba8904a1853697aa255c0b745
Stupendos <topakovdenis671@gmail.com>
2025-01-25 13:52:01 +0700

    4.4 HW Logger

--- src/main/java/org/example/databasehw/controller/InfoStudentController.java
+++ src/main/java/org/example/databasehw/controller/InfoStudentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@RequestMapping("/student")
+@RequestMapping("/info-student")
 @RestController
 public class InfoStudentController {
 
